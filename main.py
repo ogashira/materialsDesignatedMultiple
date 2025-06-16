f80d5f0673eb3c4b5fe0b41473ee9b50944db6bd
ogashira <oga@toyo-pc12.localdomain>
2025-06-16 15:39:43 +0900

    main.py if len(argv) > 0: -> if len(argv) > 1 に修正

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from program_flow import *
 def main()->None:
 
     is_a:bool = False
-    if sys.argv[1] == '-a':
-        is_a = True
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-a':
+            is_a = True
 
     program_flow:object = ProgramFlow()
     program_flow.start(is_a)
